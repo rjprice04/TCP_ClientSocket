591dd4356d3c1de7dbf2630ce7c8b1a0df94f972
Ryan <ryan-price@uiowa.edu>
2018-09-13 21:03:22 -0500

    first commit

--- TC_Socket/main.c
+++ TC_Socket/main.c
@@ -7,9 +7,28 @@
 //
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+    // creat the socket
+    int netWorkSocket;
+    netWorkSocket = socket(AF_INET, SOCK_STREAM, 0);
+    //address for the scoket
+    struct sockaddr_in serverAddress;
+    serverAddress.sin_family = AF_INET;
+    serverAddress.sin_port = htons(9002);
+    serverAddress.sin_addr.s_addr = INADDR_ANY;
+    if(connect(netWorkSocket, (struct sockaddr *) &serverAddress, sizeof(serverAddress))<0){
+        printf("Error making connection \n\n");
+    }
+    //recive
+    char serverResponce[256];
+    recv(netWorkSocket, &serverResponce, sizeof(serverResponce), 0);
+    
+    printf("%s", serverResponce);
+    close(netWorkSocket);
     return 0;
 }
